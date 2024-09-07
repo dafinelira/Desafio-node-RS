61f28d74249ace1b5fe3a24a2bd593aa7b4c79ef
dafine lira <dafinelirar@gmail.com>
2024-09-07 19:48:29 -0300

    linkando routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const app = express();
+const routes = require('./routes');
 
 app.use(express.json());
+app.use(routes);
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is runing on Port ${PORT}`));

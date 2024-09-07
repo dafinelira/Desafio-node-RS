df02eca4773ebb0fe57d5f139fd1e85483152443
dafine lira <dafinelirar@gmail.com>
2024-09-07 19:47:23 -0300

    adicionando a biblioteca express

--- src/server.js
+++ src/server.js
@@ -2,5 +2,7 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is runing on Port ${PORT}`));

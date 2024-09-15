cff8a8e697c0b9f678a8e20144fc7fc47778c26f
dafine lira <dafinelirar@gmail.com>
2024-09-15 20:19:54 -0300

    criando o banco de dados (database.bd)

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('express-async-errors');
 
+const database = require("./database/sqlite")
 const express = require('express');
 const app = express();
 
@@ -9,6 +10,8 @@ const routes = require('./routes/index.js');
 
 app.use(express.json());
 app.use(routes);
+
+database();
 
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {

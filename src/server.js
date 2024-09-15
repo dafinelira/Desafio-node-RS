b933d026eb4a27cb35f94bd9e20586ccba28a690
dafine lira <dafinelirar@gmail.com>
2024-09-15 19:36:24 -0300

    criando o app.use

--- src/server.js
+++ src/server.js
@@ -3,11 +3,29 @@ require('express-async-errors')
 const express = require('express');
 const app = express();
 
-const AppError = require('./utils/AppError')
-const routes = require('./routes/index');
+const AppError = require('./utils/AppError.js')
+const routes = require('./routes/index.js');
 
-//app.use(express.json());
-//app.use(routes);
+
+app.use(express.json());
+app.use(routes);
+
+app.use((error, request, response, next) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message
+      })
+    }
+  
+    console.error(error)
+  
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error'
+    })
+  })
+  
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is runing on Port ${PORT}`));

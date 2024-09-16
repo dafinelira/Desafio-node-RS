c87e2aeb6e9748a749db66ce9e25e7e8e2c82459
dafine lira <dafinelirar@gmail.com>
2024-09-15 21:49:05 -0300

    criando tabela de tags

---
+++ src/database/sqlite/migrations/createTags.js
@@ -0,0 +1,10 @@
+const createTags = `
+    CREATE TABLE IF NOT EXISTS tags (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movie_note_id INTEGER REFERENCES movie_notes(id) ON DELETE CASCADE,      
+    user_id INTEGER REFERENCES users(id), 
+    name_note VARCHAR
+    );
+`;
+
+module.exports = createTags;
--- src/database/sqlite/migrations/index.js
+++ src/database/sqlite/migrations/index.js
@@ -2,10 +2,11 @@ const sqliteConnection = require('../../sqlite');
 
 const createUsers = require('./createUsers');
 const createMovies = require('./createMovies');
+const createTags = require('./createTags');
 
 async function migrationsRun() {
     const schemas = [
-        createUsers, createMovies
+        createUsers, createMovies,createTags
     ].join(';\n');
     
     sqliteConnection()
